40d032c4c33d5f72d1aa17dd737c68842c8c5071
wadahideyuki <wadahideyuki@gmail.com>
2021-03-25 17:54:32 +0900

    GDG途中

--- htdocs/common/js/common.js
+++ htdocs/common/js/common.js
@@ -14,11 +14,54 @@ if(ipad == true){
   $('meta[name="viewport"]').attr("content", "width=1200px");
 }
 
+$(".ytCBox").colorbox({
+  iframe: true,
+  opacity: 0.5,
+  innerWidth: 947,
+  innerHeight: 600,
+  maxWidth:"90%",
+      maxHeight:"90%"
+});
+//画像複数
+$(".mdlImgGroup1").colorbox({
+  opacity: 0.5,
+  rel: "group1",
+  maxWidth:"90%",
+      maxHeight:"90%"
+});
 //SPのみ
 var ua = navigator.userAgent;
 if (ua.indexOf('iPhone') > 0 ||ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) || ua.indexOf('Windows Phone') > 0) {
+  
+$(".aboutBox ul").slick({
+	arrows: false,
+  adaptiveHeight: true,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
 }
 
+$(".voiceBox ul").slick({
+	arrows: true,
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  variableWidth: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+				arrows: false,
+        adaptiveHeight: true,
+        dots: true
+      }
+    }
+  ]
+});
 
 //クリックスクロール
 function clkScrl(btn, pos) {
@@ -49,29 +92,6 @@ $(".u-selectWrap select").change(function(){
 });
 
 
-$(".slkSld ul").slick({
-	arrows: true,
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  variableWidth: true,
-  centerMode: true,
-  adaptiveHeight: true,
-  responsive: [
-    {
-      breakpoint: 769,
-      settings: {
-				arrows: true,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true
-      }
-    }
-  ]
-});
 
 
 });//DocRdyFncEnd
